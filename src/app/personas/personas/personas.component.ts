3894259c600b9d15f58c1171675cb913d5f9f2d6
alejoherg21 <102333526+alejoherg21@users.noreply.github.com>
2023-03-16 07:01:30 -0500

    cambio 3

--- src/app/personas/personas/personas.component.ts
+++ src/app/personas/personas/personas.component.ts
@@ -62,7 +62,7 @@ export class PersonasComponent implements OnInit {
       estadoCivil: ['', []],
       tieneHermanos: ['', []],
     })
-      console.log('hola')
+      console.log('hola mundo')
   }
   get f(){
     return this.form.controls;
